e25d3b4650fb01974c41189cfeba3b947b5cb748
ruslankriklivyy <miminimalizm@gmail.com>
2021-08-29 12:04:25 +0300

    added fetch movies from api

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,8 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  // Global SCSS: https://go.nuxtjs.dev/config-css
+  css: ["~/styles/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -38,7 +38,7 @@ export default {
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ["@nuxt/http"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
--- pages/index.vue
+++ pages/index.vue
@@ -11,48 +11,4 @@ import Vue from "vue";
 export default Vue.extend({});
 </script>
 
-<style lang="scss">
-* {
-  box-sizing: border-box;
-  font-family: "Noto Sans JP", sans-serif;
-}
-html,
-body {
-  margin: 0;
-  padding: 0;
-}
-body {
-  background-color: #1b1d1c;
-}
-.slick-dots {
-  li.slick-active button:before {
-    background-color: #fcb124 !important;
-  }
-  li {
-    button {
-      &:before {
-        font-size: 0;
-        border-radius: 100%;
-        width: 10px;
-        height: 10px;
-        background-color: #535554 !important;
-      }
-
-      &:focus {
-        outline: none !important;
-      }
-    }
-  }
-}
-ul,
-li {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-.container {
-  max-width: 1430px;
-  margin: 0 auto;
-  padding: 0 15px;
-}
-</style>
+<style lang="scss"></style>
